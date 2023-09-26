04a57b5e0e4aba1d65e8155d96a9abb2423dbddd
Aleks <https://github.com/aleksfeschuk>
2023-09-26 23:36:03 +0200

    styles

--- task1.js
+++ task1.js
@@ -1,4 +1,4 @@
-
+// async 
 async function loadJson(url) {
     let response = await fetch(url);
         if (response.status == 200) {
@@ -11,3 +11,27 @@ async function loadJson(url) {
 
 loadJson('no-such-user.json')
     .catch(alert);
+
+
+// 2
+
+async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    return 10;
+} 
+
+function f() {
+
+    let resultPromise = wait ();
+    
+
+    resultPromise.then(result => {
+        alert(result);
+    })
+    // ...что здесь написать?
+  // чтобы вызвать wait() и дождаться результата "10" от async–функции
+  // не забывайте, здесь нельзя использовать "await"
+}
+
+f();
